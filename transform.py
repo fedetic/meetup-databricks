34eb64188f864492c9a12e3efd1e4abacbe1a129
fedetic <fedetic>
2022-06-25 10:52:25 +0000

    finished write to enriched process

--- transform.py
+++ transform.py
@@ -4,6 +4,11 @@
 
 # COMMAND ----------
 
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+# COMMAND ----------
+
 spark.conf.set("fs.azure.account.key.adlstdmeetup.dfs.core.windows.net", "54KYbLx4u1h/tkbOlAGfamyLtUFgQFLvqPVKm5vlr0rSNUgyelx5gsGM6lIgLyqSedwVFV2zPtTC+AStqSoYBQ==")
 # dbutils.fs.ls("abfss://raw@adlstdmeetup.dfs.core.windows.net/data")
 
@@ -11,45 +16,48 @@ spark.conf.set("fs.azure.account.key.adlstdmeetup.dfs.core.windows.net", "54KYbL
 
 # Set variables
 
-container_source = 'raw'
-container_destination = 'enriched'
-storage_account = 'adlstdmeetup'
+container_source = "raw"
+container_destination = "enriched"
+storage_account = "adlstdmeetup"
+tables = ["groups", "users", "events", "venues"]
 
 # COMMAND ----------
 
-# Mount /mnt/raw to raw container
-dbutils.fs.mount(
-  source = f"wasbs://{container_source}@{storage_account}.blob.core.windows.net",
-  mount_point = "/mnt/raw",
-  extra_configs = {f"fs.azure.account.key.{storage_account}.blob.core.windows.net":"54KYbLx4u1h/tkbOlAGfamyLtUFgQFLvqPVKm5vlr0rSNUgyelx5gsGM6lIgLyqSedwVFV2zPtTC+AStqSoYBQ=="})
+# # Only needed in first run
+# # Mount /mnt/raw to raw container
+# dbutils.fs.mount(
+#   source = f"wasbs://{container_source}@{storage_account}.blob.core.windows.net",
+#   mount_point = "/mnt/raw",
+#   extra_configs = {f"fs.azure.account.key.{storage_account}.blob.core.windows.net":"54KYbLx4u1h/tkbOlAGfamyLtUFgQFLvqPVKm5vlr0rSNUgyelx5gsGM6lIgLyqSedwVFV2zPtTC+AStqSoYBQ=="})
 
-# Mount /mnt/enriched to enriched container
-dbutils.fs.mount(
-  source = f"wasbs://{container_destination}@{storage_account}.blob.core.windows.net",
-  mount_point = "/mnt/enriched",
-  extra_configs = {f"fs.azure.account.key.{storage_account}.blob.core.windows.net":"54KYbLx4u1h/tkbOlAGfamyLtUFgQFLvqPVKm5vlr0rSNUgyelx5gsGM6lIgLyqSedwVFV2zPtTC+AStqSoYBQ=="})
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ### Load data from raw
-
-# COMMAND ----------
-
-table = "users"
-df = spark.read.format("json").load(f"/mnt/data/data/{table}/{table}.json")
-display(df)
+# # Mount /mnt/enriched to enriched container
+# dbutils.fs.mount(
+#   source = f"wasbs://{container_destination}@{storage_account}.blob.core.windows.net",
+#   mount_point = "/mnt/enriched",
+#   extra_configs = {f"fs.azure.account.key.{storage_account}.blob.core.windows.net":"54KYbLx4u1h/tkbOlAGfamyLtUFgQFLvqPVKm5vlr0rSNUgyelx5gsGM6lIgLyqSedwVFV2zPtTC+AStqSoYBQ=="})
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### Transformations
+# MAGIC ### Load from raw, transform and write to enriched
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ### Write to enriched
-
-# COMMAND ----------
-
-df.write.json(f"/mnt/enriched/data/{object}.json")
+# For each object in ALDS, load and write as parquet to enriched. For groups and events, do some additional transformations
+for table in tables:
+    df = spark.read.format("json").load(f"/mnt/raw/data/{table}/{table}.json")
+    
+    if table == 'users':
+        df_fact_groups = df.select("user_id", explode("memberships").alias("struct")).select("user_id", "struct.*")
+        
+        df_fact_groups.write.mode("overwrite").parquet(f"/mnt/enriched/data/fact_groups")
+        df.write.mode("overwrite").parquet(f"/mnt/enriched/data/{table}")
+        
+    elif table == 'events':
+        df_fact_events = df.select(md5(concat("group_id", "name")).alias("event_id"), explode("rsvps").alias("struct")).select("event_id", "struct.*")
+        
+        df_fact_events.write.mode("overwrite").parquet(f"/mnt/enriched/data/fact_events")
+        df.write.mode("overwrite").parquet(f"/mnt/enriched/data/{table}")
+        
+    else:
+        df.write.mode("overwrite").parquet(f"/mnt/enriched/data/{table}")
